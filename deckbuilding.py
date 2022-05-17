ab6f9f8ed115b93274689ed23797b20c9a2449bd
csafiran <csafiran@umd.edu>
2022-05-16 22:05:31 -0400

    deckbuilding.py Complete Edition
    - Docstrings
    - Named Heroes and Monsters
    - Complete menu
    - Buying and defeating works
    - deleted useless comments and methods

--- deckbuilding.py
+++ deckbuilding.py
@@ -1,15 +1,35 @@
-import math
 import random
-from tkinter import Menu
-
-from pyrsistent import discard
 
 #####
 # User Interface
 #####
 
 class BoardState:
+    """
+    Class that represents the board of the deckbuilding game.
+    
+    Attributes:
+        points (int): Representing the amount of points avalible in the 
+        point pool. When it reaches 0, the game is over. 
+        monster_cards (list): Will contain a list of monster cards that
+        can be defeated.
+        center_cards (list): Will contain a list of purchasable cards
+    """
     def __init__(self):
+        """
+            Creates and initilizes attributes.
+
+            Args:
+                self: An instance of the BoardState class.
+                points (int): Representing the amount of points avalible in the 
+                point pool. When it reaches 0, the game is over.
+                monster_cards (list): Will contain a list of monster cards that
+                can be defeated.
+                center_cards (list): Will contain a list of purchasable cards
+            
+            Side effects: Creates an instance variable
+                    
+        """
         self.points = 60
         self.monster_cards = [None for i in range(6)]
         self.center_cards = [None for i in range(6)]
@@ -19,21 +39,48 @@ class BoardState:
             self.renew_monster(i)
     
     def renew_card(self,index):
+        """
+            Creates and replaces bought cards based on their index.
+
+            Args:
+                index (int): Representing the position of the bought card. 
+                    
+        """
+        heroes = [Card("Larry,P,2,0,2"), Card("Izulu,P,3,2,0"), \
+            Card("Fin,P,4,0,3"), Card("Azule,P,6,3,0"), Card("Biki,P,7,5,0"),\
+                Card("Verdant,P,7,5,5"), Card("Cetra,P,8,6,0"),\
+                    Card("Emri the Shadowslayer,P,8,0,6"),\
+                        Card("Master Dhartha,P,8,7,0"), Card("P.R.I.M.E,P,10,9,9")]
         #switch from uniform probability
-        new_card1 = random.choices(range(1, 10),weights=[3,3,4,5,5,2,2,1,1])
+        card = random.choices(heroes,weights=[4,4,3,3,2,2,1,1,1,1])[0]
         #add new_card to the list
-        self.center_cards[index] = new_card1
-        print(new_card1)
-        return new_card1
-        
+        self.center_cards[index] = card
+         
     def renew_monster(self, index):
+        """
+            Creates and replaces defeated monsters based on their index.
+
+            Args:
+                index (int): Representing the position of the defeated monster. 
+                    
+        """
+        monster_names = {1:"Fire Spirt", 2:"Ice Golem", 3:"Goblin Brusier", \
+                         4:"Electro Wizard", 5:"Witch", 6:"Electro Giant", 7:"Dark Mega Knight", 8:"Golem", 9:"Leviathan", 10:"Ender of Worlds"}
         #switch from uniform probability
-        new_card2 = random.choices(range(1, 10),weights=[3,3,4,5,5,2,2,1,1])
+        cost = random.choices(range(1, 10),weights=[3,3,4,5,5,2,2,1,1])[0]
         #add new_card to the list
-        self.monster_cards[index] = new_card2
-        return new_card2
+        self.monster_cards[index] = Card(f"{monster_names[cost]},M,{cost},0,0") 
    
     def __str__(self):
+        """
+            Displays both, the monster row and the purchasable cards row, to the
+            user.
+
+            Side effects: Prints both rows to the user.
+            
+            Returns: The points remaining in the point pool. 
+                    
+        """
         # Display both card rows
         print(self.monster_cards)
         print(self.center_cards)
@@ -46,6 +93,17 @@ class BoardState:
 #####
 
 def remove_choice(population, k):
+    """
+        Removes the respective (monster or pruchasable) card from their
+        respective rows
+        
+        Args: 
+            population (list): Represents the card
+            k (int): The position of the selected card
+        
+        
+        Returns: The removed card from the list        
+    """
     # function for selecting removing selected card
     choice = []
     for i in range(k):
@@ -57,15 +115,44 @@ class Player:
     """Class for a deckbuilding player.
     
     Attributes:
-        name (str): the player's name.
-        money(int): the players money)
+        name (str): The player's name.
+        points (int): The player's total points.
+        path (filepath): The filepath to each player's respective deck. Stored 
+        as a text file.
+        
     """
     def __init__(self, name, path):
+        """
+            Creates and initilizes attributes.
+
+            Args:
+                self: An instance of the Player class.
+                name (string): The player's name
+                points (int): The player's total points.
+                path (filepath): The filepath to each player's respective deck. Stored 
+                as a text file.
+            
+            Side effects: Rewrites the starter deck file for each player.
+                    
+        """
         self.name = name
         self.points = 0
         self.path = path
-        self.draw_pile = []
         self.discard_pile = []
+        with open(self.path, "w", encoding = "UTF-8") as f:
+            f.write(
+            '''
+            Money Card, P, 0, 1, 0
+            Money Card, P, 0, 1, 0
+            Money Card, P, 0, 1, 0
+            Money Card, P, 0, 1, 0
+            Money Card, P, 0, 1, 0
+            Money Card, P, 0, 1, 0
+            Money Card, P, 0, 1, 0
+            Money Card, P, 0, 1, 0
+            Combat Card, P, 0, 0, 1
+            Combat Card, P, 0, 0, 1
+            ''')
     
     def turn(self, state):
         """Take a turn. Draing the player's hand and having the player make 
@@ -77,6 +164,8 @@ class Player:
         Side Effects:
             May print gamestate or other UI information
         """
+        print(f"{self.name}'s turn")
+        
         hand = self.draw()
         
         # calculate total money availble
@@ -86,7 +175,6 @@ class Player:
         combat = sum([card.combat_payout for card in hand])
 
         # display state
-        print(f"Board: {state}")
         print("Hand:")
         for card in hand:
             print(f"* {card}")
@@ -94,29 +182,54 @@ class Player:
         # UI
         cont = True
         while cont:
+            print(f"Board: {state}")
             print(f"Remaining Money: {money}")
             print(f"Remaining Combat: {combat}")
-            input(f"What to do? ")
+            decision = input(f"What are you going to do? Attack [A], Buy [B], or Change turn [C]? ")
+            if decision == 'A':
+                index = int(input("Which card are you going to attack? [1-6]")) - 1
+                card = state.monster_cards[index]
+                if card.cost > combat:
+                    print("You lack the necessary combat points!")
+                else:
+                    self.points += card.cost + 1
+                    state.renew_monster(index)
+                    state.points -= card.cost + 1
+                    combat -= card.cost
+                
+            elif decision == 'B':
+                index = int(input("Which card would you like to buy? [1-6]")) - 1
+                card = state.center_cards[index]
+                if card.cost > money:
+                    print("You lack the necessary funds!")
+                else:
+                    state.renew_card(index)
+                    money -= card.cost
+                    self.discard_pile.append(card)
+                
+            elif decision == 'C':
+                cont = False
             
-            # TODO stub
-            cont = False
+            else:
+                print("Learn to read, moron")
+                
 
         # discard hand at end of turn
         self.discard_pile.extend(hand)
-
-        # print(GameState)
-        # print(self.name + "'s hand: " + str(hand))
         
     def draw(self):
         """
-        function to draw five cards from the player's deck. Returns "cards" as
-        a list. Adds them to the player's discard pile. This function will 
-        automatically take cards from the player's discard pile when the deck
+        Function to draw five cards from the player's deck to their hand. 
+        Adds them to the player's discard pile when turn is over. This function 
+        will automatically take cards from the player's discard pile when the deck
         has zero cards and still needs to draw one or more.
+        
+        Returns: Returns "cards" as a list.
         """
 
         # load deck from file
         deck = []
+        
         with open(self.path, encoding="UTF-8") as f:
             for line in f:
                 line = line.strip()  # remove leading and trailing whitespace
@@ -135,20 +248,40 @@ class Player:
             # draw remaining cards (Note: not handling case where there are less then 5 cards between deck and discard)
             hand.extend(remove_choice(deck, 5 - len(hand)))
         
-        # save deck to file
+            # save deck to file
         with open(self.path, "w", encoding="UTF-8") as f:
             for card in deck:
                 f.write(f"{repr(card)}\n")
-        
+                
         return hand
                             
     def calc_score(self):
+        """
+        Calculates current score
+        
+        Returns:
+            The final score of the game
+        """    
         return 0
-    # TODO Stubby
+    # TODO Stubby. Zero is a test value.
 
 class Card:
+    """Class for indexing and parsing cards.
+    
+    Attributes:
+        card_string (string): The card string that we will parse.
+        
+    """
     def __init__(self, card_string):
-        print(f"Card.__init__({card_string}") #for debugging
+        """
+            Creates and initilizes attributes.
+
+            Args:
+                self: An instance of the Card class.
+                card_string (string): The card string that we will parse.
+                                
+        """
+        #print(f"Card.__init__({card_string}") for debugging
         
         #creating an iterator for parsing
         self.card_string = card_string
@@ -162,62 +295,35 @@ class Card:
         self.combat_payout = int(fields[4])
     
     def __str__(self):
+        """
+            Displays the player's name and their total money and combat for that
+            turn.
+            
+            Returns: The player's name and their total money and combat for that
+            turn. 
+                    
+        """
         return f'{self.name}: {max(self.money_payout, self.combat_payout)}'
     
     def __repr__(self):
+        """
+            
+            Returns: The parsing iterator. 
+                    
+        """
         return self.card_string
-        
-    
-    def defeat():
-        """function that will represent what happens when a player defeats a monster 
-    card from the center row
-        """
-
-    def action():
-        """function that will represent the actions the player does with their cards
-        in their hand   
-        """
-    def killed(self, pk, whichm):
-
-        if whichm.hp <= 0:
-            pk.honer += self.honer
-        return f"Now you have {pk.honer} total honers"
-        
-def test1():
-    
-    player117 = Player("John", "player1Deck.txt")
-    package  = Player("Cortana", "player2Deck.txt")
-
-    while 1 == 1:
-        print("starttest1")
-        print(player117.name)
-        print(player117.draw()) 
-        print(package.name)
-        print(package.draw())   
-        print("endtest1")   
-
-def test2():
-    #switch from uniform probability
-    #if self.center_cards < 5:
-    new_card = random.choices(range(1, 10),weights=[3,3,4,5,5,2,2,1,1])
-        #add new_card to the list
-        #self.center_cards[i] = new_card
-    print(new_card)
-    #return new_card
 
 def main():
     """
-    The main function is mainly used to call other functions and run our game.
+    The main function is where users enter their name, the GameState is 
+    constructed, the loop of the player turns, and the winners are declared.
 
-    return:
-    it will return the state of the game
-
-    Side Facts:
-    it will print the info of the game.
+    Side effects:
+    Asks for the users names and prints the winners of the game.
     """
     #Main menu: We need two player names
-    p1name = "John"
-    p2name = "Cortana"
+    p1name = input("Player 1, enter your name: ")
+    p2name = input("Player 2, enter your name: ")
     
     #Construct the GameState
     board = BoardState()
@@ -228,22 +334,18 @@ def main():
     while turn != 0 or board.points > 0:
         players[turn].turn(board)
         turn = (turn + 1) % len(players)
-    
+        
     #Declare winner (cleanup: reset temp files)
     max_score = -1
     winners = []
     for player in players:
-        score = player.calc_score()
-        if score > max_score:
-            winners = [player]
-        elif score == max_score:
-            winners.append(player)
+       score = Player.calc_score()
+       if score > max_score:
+           winners = [player]
+       elif score == max_score:
+          winners.append(player)
     print("Winner(s): " + str(winners))
     # TODO: Clean up??
 
 if __name__ == "__main__":
-    #test1()
-    #test2()
-    #BoardState()
-    #print(Player("Cyrus", "player1Deck.txt").draw())
     main()
